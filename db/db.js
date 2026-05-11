5ad0a701c6fb068dec2a4283ecca5c5f6c51155c
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-05-11 16:27:28 +0530

    feat: server setup and db connection working

---
+++ db/db.js
@@ -0,0 +1,24 @@
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Database connection failed");
+    console.log(err);
+    return;
+  }
+
+  console.log("MySQL Connected");
+});
+
+export default connection;
